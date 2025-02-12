b72ad11166c9b8c657b8f11529f61571046331e8
Caio Collete <ccollete@serradata.com>
2025-02-12 17:30:44 -0300

    functions of commands to header command.h

--- commands.h
+++ commands.h
@@ -1,1 +1,57 @@
+struct tpcommand{
+	int type;
+	char value[MAXCOMMAND]; 
+};
+typedef struct tpcommand Command;
 
+const char *prefixos[NUM_PREFIXOS] = {
+    "SET DEFAULT TO ", // 0
+    "CREATE ", // 1
+    "DIR", // 2
+    "QUIT", // 3
+    "LIST STRUCTURE", // 4
+    "APPEND", // 5
+    "LIST FOR NOME = ", // 6
+    "LIST", // 7
+    "CLEAR", // 8
+    "LOCATE FOR NOME = ", // 9
+    "GOTO ", // 10
+    "DISPLAY", //11
+    "EDIT", //12
+    "DELETE ALL", // 13
+    "DELETE", // 14
+    "RECALL", // 15
+    "SET DELETED ", // 16
+    "PACK", // 17
+    "ZAP" // 18
+};
+
+
+void to_upper_str(char *str) {
+	int i;
+    for (i = 0; str[i]; i++) {
+        str[i] = toupper((unsigned char)str[i]);
+    }
+}
+
+Command processarComando(char *entrada) {
+    Command command;
+    int i = 0;
+
+    while (i < NUM_PREFIXOS && strncmp(entrada, prefixos[i], strlen(prefixos[i])) != 0) {
+        i++;
+    }
+
+    if (i < NUM_PREFIXOS) {
+        command.type = i;
+
+        strncpy(command.value, entrada + strlen(prefixos[i]), MAXCOMMAND - 1);
+        command.value[MAXCOMMAND - 1] = '\0';
+    } 
+	else {
+        command.type = -1;
+        strcpy(command.value, "INVALID COMMAND");
+    }
+
+    return command;
+}
--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAXCOMMAND 128
+#define NUM_PREFIXOS 19
+
+#include "db.h"
+#include "commands.h"
+
+
 #include "settodefault.h"
 
 #ifdef _WIN32
@@ -12,70 +19,6 @@
     #include <unistd.h> // sleep on MacOS
     #include "./conio3/conio3.h"
 #endif
-
-
-#include "db.h"
-#include "commands.h"
-
-#define MAXCOMMAND 128
-#define NUM_PREFIXOS 19
-
-struct tpcommand{
-	int type;
-	char value[MAXCOMMAND]; 
-};
-typedef struct tpcommand Command;
-
-const char *prefixos[NUM_PREFIXOS] = {
-    "SET DEFAULT TO ", // 0
-    "CREATE ", // 1
-    "DIR", // 2
-    "QUIT", // 3
-    "LIST STRUCTURE", // 4
-    "APPEND", // 5
-    "LIST FOR NOME = ", // 6
-    "LIST", // 7
-    "CLEAR", // 8
-    "LOCATE FOR NOME = ", // 9
-    "GOTO ", // 10
-    "DISPLAY", //11
-    "EDIT", //12
-    "DELETE ALL", // 13
-    "DELETE", // 14
-    "RECALL", // 15
-    "SET DELETED ", // 16
-    "PACK", // 17
-    "ZAP" // 18
-};
-
-
-void to_upper_str(char *str) {
-    for (int i = 0; str[i]; i++) {
-        str[i] = toupper((unsigned char)str[i]);
-    }
-}
-
-Command processarComando(char *entrada) {
-    Command command;
-    int i = 0;
-
-    while (i < NUM_PREFIXOS && strncmp(entrada, prefixos[i], strlen(prefixos[i])) != 0) {
-        i++;
-    }
-
-    if (i < NUM_PREFIXOS) {
-        command.type = i;
-
-        strncpy(command.value, entrada + strlen(prefixos[i]), MAXCOMMAND - 1);
-        command.value[MAXCOMMAND - 1] = '\0';
-    } 
-	else {
-        command.type = -1;
-        strcpy(command.value, "INVALID COMMAND");
-    }
-
-    return command;
-}
 
 
 int main(void){
@@ -97,19 +40,18 @@ int main(void){
 		command=processarComando(ent);
         printf("[%d] %s\n", command.type, command.value);
         
-        switch(command.value){
+        switch(command.type){
             case 0: setDefault(&dbase, command.value, &dbAtual);
+            		printf("Defining an disk...");
                     break;
             case 1: break;
         }
         
-		
+        sleep(3); // sec
         
 #ifdef _WIN32
-        sleep(3000); // milisec
         system("cls");
 #else
-        sleep(3); // sec
         system("clear");
 #endif
 	}
