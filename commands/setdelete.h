9633e4116f465d1ddee4d1ae8c3911a8c4719d53
caiocollete <caiocolletesilva@gmail.com>
2025-03-18 13:00:30 -0300

    setdeleted fixed

--- commands/setdelete.h
+++ commands/setdelete.h
@@ -1,11 +1,16 @@
-void setdelete(int *viewDelete, char *value){
-	if(*viewDelete == NULL || value==NULL){
-		printf("Missing parameters");
-	}
-	else{
-		if(strcmp(value,"ON")==0)
-			*viewDelete=1;
-		else 
-			*viewDelete=0;
-	}
+void setdelete(int *viewDelete, char *value) {
+    if (viewDelete == NULL || value == NULL) {
+        printf("Missing parameters\n");
+    }
+    else{
+      	if(strcmp(value, "ON") == 0){
+	       *viewDelete = 1;
+	       printf("viewDelete ON\n");
+	    }
+		else {
+	        *viewDelete = 0;
+	        printf("viewDelete OFF\n");
+	    }
+    }
 }
+
